bff0a0906b8ec7f1fa14baf55c411fe59837c253
BartoszRumak <27711689+RumakBartosz@users.noreply.github.com>
2020-04-25 14:43:03 +0200

    game_over function implementation for minimax bot

--- src/negamax/mod.rs
+++ src/negamax/mod.rs
@@ -1,9 +1,8 @@
-#[path = "../parser/mod.rs"] pub mod parser;
-use parser::{Mark, Color,
+use crate::parser::{Color,
              is_move_up_possible, is_move_down_possible,
              is_move_left_possible, is_move_right_possible};
 
-use parser::Mark::*;
+use crate::parser::Mark::{self,x};
 
 
 pub struct MiniMaxBot {
@@ -19,27 +18,31 @@ impl MiniMaxBot {
         }
     }
 
-    fn game_over(&self) -> bool {
+    pub fn game_over(&self) -> bool {
+        if self.get_all_available_moves(Color::Red).len() == 0 {
+            return true;
+        }
+
+        if self.get_all_available_moves(Color::Blue).len() == 0 {
+            return true;
+        }
+
         false
     }
 
-    fn get_all_available_moves(&self) -> Vec<&str> {
+    fn get_all_available_moves(&self, color: Color) -> Vec<&str> {
         let mut available_move: Vec<&str> = Vec::new();
-        if is_move_up_possible(&self.map, &self.color) {
+        if is_move_up_possible(&self.map, &color) {
             available_move.push("up");
         }
-        if is_move_down_possible(&self.map, &self.color) {
+        if is_move_down_possible(&self.map, &color) {
             available_move.push("down");
         }
-        if is_move_left_possible(&self.map, &self.color) {
+        if is_move_left_possible(&self.map, &color) {
             available_move.push("left");
         }
-        if is_move_right_possible(&self.map, &self.color) {
+        if is_move_right_possible(&self.map, &color) {
             available_move.push("right");
-        }
-
-        if available_move.len() == 0 {
-            available_move.push("up");
         }
 
         available_move
@@ -49,7 +52,7 @@ impl MiniMaxBot {
         vec![vec![x]]
     }
 
-    fn unmake_move(one_move: &str) -> Vec<Vec<Mark>> {
+    fn unmake_move(one_move: &str) -> Vec<Vec<Mark>> 
         vec![vec![x]]
     }
 }
